66551be03e2c30a0cf7bf06f81bd95b61043cc92
Justyna Rachowicz <mrowa44@gmail.com>
2017-10-31 17:10:50 +0100

    Add apollo setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { HttpLink, InMemoryCache } from 'apollo-client-preset';
 import 'normalize.css';
 
 import App from 'components/App';
 import registerServiceWorker from 'utils/registerServiceWorker';
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: '/graphql' }),
+  cache: new InMemoryCache().restore({}),
+});
+
 function render(Component) {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <ApolloProvider client={client}>
+        <Component />
+      </ApolloProvider>
     </AppContainer>,
     document.getElementById('root'),
   );
